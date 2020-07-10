1efa9182cbbdcdfa4b41dd11a76dd8b5cc3caf8c
Saul Maldonado <samaldonado4@gmail.com>
2020-07-10 17:51:50 -0500

    Initialized LinkedList class

---
+++ LinkedList.ts
@@ -0,0 +1,10 @@
+class Node {
+  private value: number;
+  private next: Node | null = null;
+}
+
+class LinkedList {
+  private head: Node | null = null;
+  private tail: Node | null = null;
+  constructor() {}
+}
