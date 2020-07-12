e63af8d8eb1e15571d76cfff9a17224decda5e23
Saul Maldonado <samaldonado4@gmail.com>
2020-07-12 18:43:34 -0500

    Optimize BubbleSort

--- Sort.ts
+++ Sort.ts
@@ -1,15 +1,19 @@
 class Sort {
   static BubbleSort(arr: number[]): number[] {
     if (arr.length < 2) return arr;
+    let isSorted: boolean;
 
     for (let j = 0; j < arr.length; j++) {
-      for (let i = 1; i < arr.length; i++) {
+      isSorted = true;
+      for (let i = 1; i < arr.length - j; i++) {
         if (arr[i - 1] > arr[i]) {
           let larger = arr[i - 1];
           arr[i - 1] = arr[i];
           arr[i] = larger;
+          isSorted = false;
         }
       }
+      if (isSorted) break;
     }
 
     return arr;
