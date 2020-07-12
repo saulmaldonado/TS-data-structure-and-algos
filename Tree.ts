d22945684b70f65a44f7e0e713c957cf915d68cb
Saul Maldonado <samaldonado4@gmail.com>
2020-07-12 14:18:52 -0500

    Fix Tree insert method and implement preorder traversal

--- Tree.ts
+++ Tree.ts
@@ -12,16 +12,24 @@ class Tree {
   root: Node | null = null;
 
   insert(value: number) {
+    if (!this.root) return (this.root = new Node(value));
+
     let currentNode: Node | null = this.root;
 
     while (currentNode !== null) {
       if (value < currentNode.value) {
+        if (currentNode.leftChild === null) {
+          return (currentNode.leftChild = new Node(value));
+        }
         currentNode = currentNode.leftChild;
       } else {
+        if (currentNode.rightChild === null) {
+          return (currentNode.rightChild = new Node(value));
+        }
         currentNode = currentNode.rightChild;
       }
     }
-    currentNode = new Node(value);
+    return (currentNode = new Node(value));
   }
 
   find(value: number) {
@@ -36,5 +44,13 @@ class Tree {
         currentNode = currentNode.rightChild;
       }
     }
+    return false;
+  }
+
+  traversePreorder(root: Node | null = this.root) {
+    if (!root) return;
+    console.log(root.value);
+    this.traversePreorder(root.leftChild);
+    this.traversePreorder(root.rightChild);
   }
 }
