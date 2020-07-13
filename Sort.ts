df633e91446e76664247eb24b4073fde744e068c
Saul Maldonado <samaldonado4@gmail.com>
2020-07-12 20:32:07 -0500

    Implement SelectionSort algorithm

--- Sort.ts
+++ Sort.ts
@@ -1,4 +1,5 @@
 class Sort {
+  // worst case O^2
   static BubbleSort(arr: number[]): number[] {
     if (arr.length < 2) return arr;
     let isSorted: boolean;
@@ -16,6 +17,21 @@ class Sort {
       if (isSorted) break;
     }
 
+    return arr;
+  }
+
+  static SelectionSort(arr: number[]) {
+    for (let i = 0; i < arr.length; i++) {
+      let minIndex = i;
+      for (let j = i; j < arr.length; j++) {
+        if (arr[minIndex] > arr[j]) {
+          minIndex = j;
+        }
+      }
+      let smaller = arr[minIndex];
+      arr[minIndex] = arr[i];
+      arr[i] = smaller;
+    }
     return arr;
   }
 }
