4475688ab047a8fadffb22a5f438a8a7303f0f1c
Saul Maldonado <samaldonado4@gmail.com>
2020-07-13 20:14:28 -0500

    Implement recursive binary search method

--- Search.ts
+++ Search.ts
@@ -5,4 +5,27 @@ class Search {
     }
     return -1;
   }
+
+  static BinarySearchRecursive(
+    arr: number[],
+    item: number,
+    left: number = 0,
+    right: number = arr.length - 1,
+  ): number {
+    let middle = Math.floor((left + right) / 2);
+
+    if (left > right) {
+      return -1;
+    }
+
+    if (arr[middle] === item) {
+      return middle;
+    }
+
+    if (item < arr[middle]) {
+      return Search.BinarySearchRecursive(arr, item, left, middle - 1);
+    }
+
+    return Search.BinarySearchRecursive(arr, item, middle + 1, right);
+  }
 }
