faf6144bf2ce940db9628050790bf216a8d62201
Saul Maldonado <samaldonado4@gmail.com>
2020-07-13 21:55:40 -0500

    Implement TernarySearch algorithm

--- Search.ts
+++ Search.ts
@@ -28,4 +28,50 @@ class Search {
 
     return Search.BinarySearchRecursive(arr, item, middle + 1, right);
   }
+
+  static BinarySearchIterative(arr: number[], item: number) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+      let middle = Math.floor((left + right) / 2);
+      if (arr[middle] === item) {
+        return middle;
+      }
+
+      if (item < arr[middle]) {
+        right = middle - 1;
+      } else {
+        left = middle + 1;
+      }
+    }
+    return -1;
+  }
+
+  static TernarySearch(
+    arr: number[],
+    item: number,
+    left: number = 0,
+    right: number = arr.length - 1,
+  ): number {
+    if (left > right) return -1;
+
+    let partitionSize = Math.floor((right - left) / 3);
+
+    let mid1 = left + partitionSize;
+    let mid2 = right + partitionSize;
+
+    if (arr[mid1]) return mid1;
+
+    if (arr[mid2]) return mid2;
+
+    if (item < arr[mid1]) {
+      return Search.TernarySearch(arr, item, left, mid1 - 1);
+    }
+    if (item > arr[mid2]) {
+      return Search.TernarySearch(arr, item, mid2 + 1, right);
+    }
+
+    return Search.TernarySearch(arr, item, mid1 + 1, mid2 - 1);
+  }
 }
