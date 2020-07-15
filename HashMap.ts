448240c7335f711417f933c2f3233a5918e1406e
Saul Maldonado <samaldonado4@gmail.com>
2020-07-15 00:04:20 -0500

    Implement HashMap class

---
+++ HashMap.ts
@@ -0,0 +1,29 @@
+class HashMap<T = any> {
+  private keyMap: Array<Array<[string, T]>> = [];
+  private prime: number = 31;
+  constructor(size: number = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  private hash(key: string) {
+    let total = 0;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      total += key[i].charCodeAt(0) * this.prime;
+    }
+    return total % this.keyMap.length;
+  }
+
+  set(key: string, value: T) {
+    const index = this.hash(key);
+    this.keyMap[index] ?? (this.keyMap[index] = new Array<[string, T]>()).push([key, value]);
+  }
+  get(key: string) {
+    const index = this.hash(key);
+    for (let i = 0; i < this.keyMap[index].length; i++) {
+      if (this.keyMap[index][i][0] === key) {
+        return this.keyMap[index][i][1];
+      }
+    }
+    return;
+  }
+}
